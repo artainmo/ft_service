d84edfcf2c86df23bd311b67d435fd402f008a4e
artainmo <tainmontarthur@icloud.com>
2020-06-19 12:21:17 +0200

    base Dockerfile phpmyadmin and mysql

--- ft_service/srcs/mysql/wordpress_database.sql
+++ ft_service/srcs/mysql/wordpress_database.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE wordpress;
 CREATE USER 'artainmo'@'localhost' IDENTIFIED BY 'pass123';
+CREATE USER 'Geoffrey'@'localhost' IDENTIFIED BY 'pass456';
+CREATE USER 'Louis'@'localhost' IDENTIFIED BY 'pass789';
 GRANT ALL PRIVILEGES ON *.* TO 'artainmo'@'localhost' IDENTIFIED BY 'pass123';
