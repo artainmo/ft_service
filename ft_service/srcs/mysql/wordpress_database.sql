4f2870282846af9bfde838c35753923ee10f2394
Arthur Tainmont <artainmo@fu-r1-p1.s19.be>
2020-09-28 15:45:08 +0200

    yes

--- ft_service/srcs/mysql/wordpress_database.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE wordpress;
-CREATE USER 'artainmo'@'localhost' IDENTIFIED BY 'pass123';
-CREATE USER 'Geoffrey'@'localhost' IDENTIFIED BY 'pass456';
-CREATE USER 'Louis'@'localhost' IDENTIFIED BY 'pass789';
-GRANT ALL PRIVILEGES ON *.* TO 'artainmo'@'localhost' IDENTIFIED BY 'pass123';
