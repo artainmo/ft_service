cbc44c6bf842f127b73287e99484ac8eefaa15b5
Arthur Tainmont <artainmo@fu-r3-p1.s19.be>
2020-09-29 18:41:41 +0200

    yes

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -1,7 +1,50 @@
+#Installing kubectl for the master node commands and minikub to run the cluster on it
+brew install kubectl
+brew install minikube
+
+#Run minikube on the host instead of one a VM, use --driver=none, now a cluster has been created
+minikube start --driver=none
+#Test if everythin is working
+#To clear minikube local state use minikube delete and to stop the cluster minikube stop
+minikube status
 
 
-#docker build --tag img . (tag to give a name to the created image)
-#docker run -i -d -p 443:443 -p 80:80 img (run to create a container, -i to keep interactive, -d run in background)
-#See docker ps -> Shows the running container
-#See result on local port http://127.0.0.1/
-#Docker kill to kill the container and system prune to kill all images
+
+#nginx
+docker build --tag nginx_image .
+kubectl create nginx --image=nginx_image
+kubectl expose nginx --type=loadbalancer --port=80 --port=443
+
+
+#worpdpress
+docker build
+kubectl create
+
+
+#influxdb
+docker build
+kubectl create
+
+
+#phpmyadmin
+docker build
+kubectl create
+
+
+#FTPS
+docker build
+kubectl create
+
+
+#Grafana
+docker build
+kubectl create
+
+
+#Load balancer
+docker build
+kubectl create
+
+
+#Launch the online kubernetes platform
+Minikube dashboard
