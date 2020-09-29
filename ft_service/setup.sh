2ed596f0cdf85d0be683a8ece39c5a67633b6e41
artainmo <tainmontarthur@icloud.com>
2020-09-29 19:56:34 +0200

    remove

--- ft_service/setup.sh
+++
@@ -1,7 +0,0 @@
-
-
-#docker build --tag img . (tag to give a name to the created image)
-#docker run -i -d -p 443:443 -p 80:80 img (run to create a container, -i to keep interactive, -d run in background)
-#See docker ps -> Shows the running container
-#See result on local port http://127.0.0.1/
-#Docker kill to kill the container and system prune to kill all images
