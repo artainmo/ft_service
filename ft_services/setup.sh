d74c5b95f9a270af831d6c3b997199dcd1c7b3bb
Arthur Tainmont <artainmo@mi-r7-p1.s19.be>
2020-10-11 03:59:53 +0200

    modifs

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -36,6 +36,26 @@ minikube addons enable metallb
 kubectl apply -f srcs/metallb.yaml
 #configure metallb with configmap object
 
+
+#!/bin/bash
+# see what changes would be made, returns nonzero returncode if different
+# kubectl get configmap kube-proxy -n kube-system -o yaml | \
+# sed -e "s/strictARP: false/strictARP: true/" | \
+# kubectl diff -f - -n kube-system
+# # actually apply the changes, returns nonzero returncode on errors only
+# kubectl get configmap kube-proxy -n kube-system -o yaml | \
+# sed -e "s/strictARP: false/strictARP: true/" | \
+# kubectl apply -f - -n kube-system
+# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+# # On first install only
+# kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+# kubectl apply -f metallb-configmap.yaml
+
+
+
+
+
 #Enable the dashboard
 minikube addons enable dashboard
 #Some dashboard features require the metrics-server addon
